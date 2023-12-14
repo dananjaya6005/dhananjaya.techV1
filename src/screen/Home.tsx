c840ee86e039f91ca1aeedb0e128bcad39bd1218
dananjaya6005 <dananjaya6005@gmail.com>
2023-12-14 23:29:53 +0530

    update cv

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -84,7 +84,7 @@ export default function Home() {
             </button>
 
             <button
-              onClick={()=>{window.open("https://raw.githubusercontent.com/dananjaya6005/dhananjaya.techV1/main/src/postImg/Resume%20Dhananjaya%20Chathuranga.pdf")}}
+              onClick={()=>{window.open("https://github.com/dananjaya6005/dhananjaya.tech/blob/main/src/files/Resume%20Dhananjaya%20Chathuranga.pdf")}}
               type="button"
               className="text-gray-900  bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2"
             >
